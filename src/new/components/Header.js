0c7deac89d5eeac720db3dc7e3a45a115af3c70a
wildpow <element1028@gmail.com>
2019-10-25 10:58:06 -0700

    updates to header

--- src/new/components/Header.js
+++ src/new/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Flex, Heading } from "rebass";
+import Tippy from "@tippy.js/react";
+import { Flex, Heading, Image } from "rebass";
 import Headroom from "react-headroom";
 import Fade from "react-reveal/Fade";
 import styled from "@emotion/styled";
 import { SectionLinks } from "react-scroll-section";
 import RouteLinks from "./RouteLink";
-import Logo from "../../images/icon.png";
+import Logo from "../../images/face2.png";
 const HeaderContainer = styled(Headroom)`
   .headroom--pinned {
     background: ${props => props.theme.colors.primaryDark};
@@ -28,7 +29,29 @@ const Header = () => (
           {({ allLinks }) => {
             return (
               <>
-                <RouteLinks {...allLinks.home} name="Aaron Wilder" />
+                <Tippy content="Home" placement="right" trigger="mouseenter">
+                  <Image
+                    src={Logo}
+                    width="50px"
+                    alt="Portfolio Logo"
+                    onClick={allLinks.home.onClick}
+                    sx={{
+                      borderRadius: "50%",
+                      border: "4px solid",
+                      borderColor: "white",
+                      transition: " all 0.25s ease-out",
+
+                      ":hover": {
+                        borderRadius: "20%"
+                      }
+                    }}
+                    style={{
+                      cursor: "pointer"
+                    }}
+                  />
+                </Tippy>
+
+                {/* <RouteLinks {...allLinks.home} name="Aaron Wilder" /> */}
                 <Flex mr={[0, 3, 5]}>
                   <RouteLinks {...allLinks.about} name="About" />
                   <RouteLinks {...allLinks.projects} name="Projects" />
